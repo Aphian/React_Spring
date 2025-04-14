9172fa7548c5b030522b80fe99b9bcd470672d98
Sang <rsw7876@naver.com>
2025-04-14 21:22:20 +0900

    spring_paging_responseDTO

--- backend2/apiserver/src/main/java/org/zerock/mallapi/dto/PageResponseDTO.java
+++ backend2/apiserver/src/main/java/org/zerock/mallapi/dto/PageResponseDTO.java
@@ -1,7 +1,12 @@
 package org.zerock.mallapi.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import lombok.Data;
+
+@Data
 public class PageResponseDTO<E> {
 
     private List<E> dtoList;
@@ -15,6 +20,28 @@ public class PageResponseDTO<E> {
     private int totalCount, prvePage, nextPage, totoalPage, currentPage;
 
     public PageResponseDTO(List<E> dtoList, PageRequestDTO pageRequestDTO, long total) {
+
+        this.dtoList = dtoList;
+        this.pageRequestDTO = pageRequestDTO;
+        this.totalCount = (int) total;
+
+        // 끝 페이지 계산
+        int end = (int) (Math.ceil(pageRequestDTO.getPage() / 10.0)) * 10;
+
+        int start = end - 9;
+
+        // total 고려 end 페이지
+        int last = (int) (Math.ceil(totalCount / (double) pageRequestDTO.getSize()));
+
+        end = end > last ? last : end;
+
+        this.prev = start > 1;
+        this.next = totalCount > end * pageRequestDTO.getSize();
+
+        this.pageNumList = IntStream.range(start, end).boxed().collect(Collectors.toList());
+
+        this.prvePage = prev ? start - 1 : 0;
+        this.nextPage = next ? end + 1 : 0;
 
     }
 
