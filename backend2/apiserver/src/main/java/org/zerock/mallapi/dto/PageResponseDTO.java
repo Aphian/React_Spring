ef3ebd7c4f3c78f4c810647f2e75ab934b616566
Sang <rsw7876@naver.com>
2025-04-14 21:03:21 +0900

    spring_pagingDTO

--- backend2/apiserver/src/main/java/org/zerock/mallapi/dto/PageResponseDTO.java
+++ backend2/apiserver/src/main/java/org/zerock/mallapi/dto/PageResponseDTO.java
@@ -1,5 +1,21 @@
 package org.zerock.mallapi.dto;
 
+import java.util.List;
+
 public class PageResponseDTO<E> {
+
+    private List<E> dtoList;
+
+    private List<Integer> pageNumList;
+
+    private PageRequestDTO pageRequestDTO;
+
+    private boolean prev, next;
+
+    private int totalCount, prvePage, nextPage, totoalPage, currentPage;
+
+    public PageResponseDTO(List<E> dtoList, PageRequestDTO pageRequestDTO, long total) {
+
+    }
 
 }
