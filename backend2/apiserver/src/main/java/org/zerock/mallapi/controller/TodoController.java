e4619784583f10e4c068f657db66a5b751fad79c
Sang <rsw7876@naver.com>
2025-04-18 14:21:12 +0900

    spring_update_delete

--- backend2/apiserver/src/main/java/org/zerock/mallapi/controller/TodoController.java
+++ backend2/apiserver/src/main/java/org/zerock/mallapi/controller/TodoController.java
@@ -2,9 +2,11 @@ package org.zerock.mallapi.controller;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +48,26 @@ public class TodoController {
         Long tno = todoService.register(dto);
 
         return Map.of("TNO", tno);
+
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable("tno") Long tno, @RequestBody TodoDTO todoDTO) {
+
+        todoDTO.setTno(tno);
+
+        todoService.modify(todoDTO);
+
+        return Map.of("RESULT", "SUCCESS");
+
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable Long tno) {
+
+        todoService.remove(tno);
+
+        return Map.of("RESULT", "SUCCESS");
 
     }
 
