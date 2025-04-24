1675f29d432cd16905b03d8ace4b49303dc423c6
Sang <rsw7876@naver.com>
2025-04-24 16:19:44 +0900

    react_list_component

--- frontend2/mall/src/pages/todo/ListPage.js
+++ frontend2/mall/src/pages/todo/ListPage.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
+import ListComponent from '../../components/todo/ListComponent'
 
 function ListPage() {
 
-  const [queryParams] = useSearchParams()
+  // const [queryParams] = useSearchParams()
 
-  // queryString 처리
-  const page = queryParams.get('page') ? parseInt(queryParams.get('page')) : 1
-  const size = queryParams.get('size') ? parseInt(queryParams.get('size')) : 10
+  // // queryString 처리
+  // const page = queryParams.get('page') ? parseInt(queryParams.get('page')) : 1
+  // const size = queryParams.get('size') ? parseInt(queryParams.get('size')) : 10
 
   return (
     <div className ="p-4 w-full bg-orange 200 ">
         <div className ="text-3xl font-extrabold">
-            Todo List Page Component --- {page} --- {size}
+            Todo List Page Component 
         </div>
+        <ListComponent></ListComponent>
     </div>
   )
 }
