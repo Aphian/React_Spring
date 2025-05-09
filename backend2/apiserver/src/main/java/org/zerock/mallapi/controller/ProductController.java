abbf82bafaed1fbbac8eb065d3f5c25aad6a5884
Sang <rsw7876@naver.com>
2025-05-09 17:04:49 +0900

    spring_file_read

--- backend2/apiserver/src/main/java/org/zerock/mallapi/controller/ProductController.java
+++ backend2/apiserver/src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -3,6 +3,10 @@ package org.zerock.mallapi.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +39,13 @@ public class ProductController {
         log.info(uploadFiledNames);
 
         return Map.of("RESULT", "SUCCESS");
+
+    }
+
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable("fileName") String fileName) {
+
+        return fileUtil.getFile(fileName);
 
     }
 
