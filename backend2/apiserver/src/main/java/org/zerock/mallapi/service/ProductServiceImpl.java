6a2d7ecd27b7479f88aa5bb3eb7c82c26b6888d8
Sang <rsw7876@naver.com>
2025-05-15 17:04:57 +0900

    spring_service_test

--- backend2/apiserver/src/main/java/org/zerock/mallapi/service/ProductServiceImpl.java
+++ backend2/apiserver/src/main/java/org/zerock/mallapi/service/ProductServiceImpl.java
@@ -1,7 +1,6 @@
 package org.zerock.mallapi.service;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
