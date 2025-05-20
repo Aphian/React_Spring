79ac0334aacef451f7f083f16be75f21254d7b06
Sang <rsw7876@naver.com>
2025-05-20 21:52:47 +0900

    product_file_update

--- backend2/apiserver/src/main/java/org/zerock/mallapi/controller/ProductController.java
+++ backend2/apiserver/src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -2,12 +2,14 @@ package org.zerock.mallapi.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -82,6 +84,44 @@ public class ProductController {
     public ProductDTO read(@PathVariable("pno") Long pno) {
 
         return productService.get(pno);
+
+    }
+
+    // Put 방식 -> 페이로드 가능
+    @PutMapping("/{pno}")
+    public Map<String, String> modify(@PathVariable("pno") Long pno, ProductDTO productDTO) {
+
+        productDTO.setPno(pno);
+
+        // 원래 상품 --> 어떤 파일들이 지워졌는지 알기 위해 DB에 저장되어 있는 정보
+        ProductDTO oldProductDTO = productService.get(pno);
+
+        // file upload
+        List<MultipartFile> files = productDTO.getFiles();
+        List<String> currentUploadFileNames = fileUtil.saveFiles(files);
+
+        // keep files --> 유지하려는 파일 / String
+        List<String> uploadedFileNames = productDTO.getUploadFileNames();
+
+        if (currentUploadFileNames != null && !currentUploadFileNames.isEmpty()) {
+
+            uploadedFileNames.addAll(currentUploadFileNames);
+
+        }
+
+        productService.modify(productDTO);
+
+        List<String> oldFileNames = oldProductDTO.getUploadFileNames();
+        if (oldFileNames != null && oldFileNames.size() > 0) {
+            // 지워저야할 file
+            List<String> removeFiles = oldFileNames.stream()
+                    .filter(fileName -> uploadedFileNames.indexOf(fileName) == -1).collect(Collectors.toList());
+
+            fileUtil.deleteFiles(removeFiles);
+
+        } // end if
+
+        return Map.of("RESULT", "SUCCESS");
 
     }
 
