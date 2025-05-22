845a6b62bc0cfb42d77a107536693062d4a6db9b
Sang <rsw7876@naver.com>
2025-05-22 14:12:27 +0900

    React_Spring_TypeScript

---
+++ frontend/src/router/productsRouter.tsx
@@ -0,0 +1,26 @@
+import { lazy, Suspense } from "react"
+import { Navigate} from "react-router"
+
+const ProductsIndex = lazy(() => import("../pages/products/indexPage"))
+const Loading = () => <div>Products Loading....</div>
+
+const ProductsList = lazy(() => import("../pages/products/listPage"))
+
+export default function productsRouter() {
+    return (
+        {
+            path: "products",
+            Component: ProductsIndex,
+            children: [
+                {
+                    path: "list",
+                    element: <Suspense fallback={<Loading></Loading>}><ProductsList></ProductsList></Suspense>
+                },
+                {
+                    path: "",
+                    element: <Navigate to={"/product/list"}></Navigate>
+                }
+            ] 
+        }
+    )
+}
