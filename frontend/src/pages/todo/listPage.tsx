7c1bdd05a7b5d829a649c6999d22a18f1bbb9ccc
Sang <rsw7876@naver.com>
2025-05-22 22:44:12 +0900

    react_pagecomponent

--- frontend/src/pages/todo/listPage.tsx
+++ frontend/src/pages/todo/listPage.tsx
@@ -1,25 +1,13 @@
-import { useSearchParams } from "react-router";
-
+import ListComponent from "../../components/todo/listComponent";
 
 function ListPage() {
-
-  const [queryParams] = useSearchParams()
-  
-  const page: string | null = queryParams.get("page")
-  const size: string | null = queryParams.get("size")
   
   return ( 
-  <div className="bg-white w-full">
-   <div className="text-4xl">Todo List Page {page} {size}</div>
-  </div>
- );
-
-
-  // return ( 
-  //   <div className="bg-white w-full">
-  //     <div className="text-4xl">Todo List Page</div>
-  //   </div>
-  //  );
+    <div className="bg-white w-full">
+      <div className="text-3xl font-extrabold">Todo List Page Component</div>
+      <ListComponent></ListComponent>
+    </div>
+  );
 }
 
 export default ListPage;
