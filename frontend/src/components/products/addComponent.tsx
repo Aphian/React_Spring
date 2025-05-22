170b7ad6f35a1cc3c3564f2b2363e75a4fee4112
Sang <rsw7876@naver.com>
2025-05-22 21:54:56 +0900

    react_pendingmodal

---
+++ frontend/src/components/products/addComponent.tsx
@@ -0,0 +1,86 @@
+import axios from "axios";
+import { useActionState } from "react";
+
+interface ProductAddResult {
+    result? : number,
+    error? : string 
+}
+
+const initState: ProductAddResult = {
+    result: 0
+}
+
+// action 처리 함수.
+const addAsyncAction = async (state: ProductAddResult, formData: FormData): Promise<ProductAddResult> => {
+
+    console.log("addAsyncAction ......")
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    const pname = formData.get("pname") as string
+
+    if(!pname) {
+        return {error: "Insert Product Name"}
+    }
+    const res = await axios.post('http://localhost:8080/api/products/', formData)
+
+    return {result: res.data.result}
+
+}
+
+
+function AddComponent() {
+
+    const [state, action, isPending] = useActionState(addAsyncAction, initState)
+
+    return (
+        <div className = "border-2 border-sky-200 mt-10 m-2 p-4"> 
+            {isPending && <div className="bg-amber-400">처리 중 ....</div>}
+            {state.result != 0 && <div>처리 결과 {state.result}</div>}
+            <form action={action} >
+                <div className="flex justify-center">
+                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                        <div className="w-1/5 p-6 text-right font-bold">Product Name</div>
+                        <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md" 
+                            name="pname" required>
+                        </input>
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                        <div className="w-1/5 p-6 text-right font-bold">Desc</div>
+                        <textarea className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md resize-y"
+                            name="pdesc" rows={4} required>
+                        </textarea>
+                    </div> 
+                </div>
+                <div className="flex justify-center">
+                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                        <div className="w-1/5 p-6 text-right font-bold">Price</div>
+                        <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md" name="price" type={'number'} required>
+                        </input>
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                        <div className="w-1/5 p-6 text-right font-bold">Files</div>
+                        <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md" 
+                            type={'file'}
+                            name="files"
+                            multiple={true}>  
+                        </input>
+                    </div>
+                </div>
+                <div className="flex justify-end">
+                    <div className="relative mb-4 flex p-4 flex-wrap items-stretch">
+                        <button type="submit" className="rounded p-4 w-36 bg-blue-500 text-xl text-white">
+                            ADD
+                        </button>    
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default AddComponent
--- frontend/src/pages/todo/listPage.tsx
+++ frontend/src/pages/todo/listPage.tsx
@@ -15,11 +15,11 @@ function ListPage() {
  );
 
 
-  return ( 
-    <div className="bg-white w-full">
-      <div className="text-4xl">Todo List Page</div>
-    </div>
-   );
+  // return ( 
+  //   <div className="bg-white w-full">
+  //     <div className="text-4xl">Todo List Page</div>
+  //   </div>
+  //  );
 }
 
 export default ListPage;
